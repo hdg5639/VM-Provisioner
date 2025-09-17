563a5fd9e777e4991e16b0af053981c2b448b04b
donggyun-han <hdg5639@daum.net>
2025-09-17 21:09:44 +0900

    Chore(IB): 프로젝트 생성

---
+++ identity_bridge/src/main/java/cloud/gamja/identity_bridge/IdentityBridgeApplication.java
@@ -0,0 +1,13 @@
+package cloud.gamja.identity_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityBridgeApplication.class, args);
+    }
+
+}
---
+++ identity_bridge/src/test/java/cloud/gamja/identity_bridge/IdentityBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.gamja.identity_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
