a17bdb50383964b316e64fb413f2bf50536210a8
donggyun-han <hdg5639@daum.net>
2025-10-01 22:27:18 +0900

    fix(vm): postgresql 교체 - 수정되지 않은 service 코드 수정

--- Backend/vm/src/main/java/cloud/gamja/vm/vmevent/VmEventService.java
+++ Backend/vm/src/main/java/cloud/gamja/vm/vmevent/VmEventService.java
@@ -3,43 +3,45 @@ package cloud.gamja.vm.vmevent;
 import cloud.gamja.vm.vmevent.domain.VmEvent;
 import cloud.gamja.vm.vmevent.enums.Actions;
 import cloud.gamja.vm.vmevent.record.EventInfo;
+import cloud.gamja.vm.vms.VmRepository;
 import cloud.gamja.vm.vms.domain.Vm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class VmEventService {
     private final VmEventRepository vmEventRepository;
+    private final VmRepository vmRepository;
 
-    public Mono<EventInfo> record(Vm vm, UUID actorUserId, Actions action, Map<String, Object> payload) {
+    public Mono<EventInfo> record(Vm vm, UUID actorUserId, Actions action, String payload) {
         return vmEventRepository.save(VmEvent.builder()
-                .vm(vm)
+                .vmId(vm.getId())
                 .actorUserId(actorUserId)
                 .action(action)
                 .payload(payload)
                 .build())
-                .map(this::convert);
+                .flatMap(this::convert);
 
     }
 
     public Flux<EventInfo> getVmEvents(Vm vm) {
         return vmEventRepository.findByVm(vm)
-                .map(this::convert);
+                .flatMap(this::convert);
     }
 
-    private EventInfo convert(VmEvent vmEvent) {
-        return new EventInfo(
-                vmEvent.getVm().getDetail().vmid(),
-                vmEvent.getActorUserId(),
-                vmEvent.getAction(),
-                vmEvent.getPayload(),
-                vmEvent.getTimestamp()
-        );
+    private Mono<EventInfo> convert(VmEvent vmEvent) {
+        return vmRepository.findById(vmEvent.getVmId())
+                .map(vm -> new EventInfo(
+                        vm.getDetail().vmid(),
+                        vmEvent.getActorUserId(),
+                        vmEvent.getAction(),
+                        vmEvent.getPayload(),
+                        vmEvent.getTimestamp()
+                ));
     }
 }
