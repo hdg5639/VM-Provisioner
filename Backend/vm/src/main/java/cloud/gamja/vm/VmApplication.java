2f53dfc7033bf28d55683c53bd171e94b807e278
donggyun-han <hdg5639@daum.net>
2025-10-03 22:30:51 +0900

    fix(vm): application EnableR2dbcRepositories 명시 - Application 어노테이션 명시

--- Backend/vm/src/main/java/cloud/gamja/vm/VmApplication.java
+++ Backend/vm/src/main/java/cloud/gamja/vm/VmApplication.java
@@ -2,10 +2,12 @@ package cloud.gamja.vm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableR2dbcRepositories(basePackages = "cloud.gamja")
 public class VmApplication {
 
     public static void main(String[] args) {
